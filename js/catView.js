d3ece3e9b0e4812b21ce8e879c47edd71ca6f357
gfatima <37168871+gfatima@users.noreply.github.com>
2021-04-06 15:32:46 -0500

    Version 1.0

---
+++ js/catView.js
@@ -0,0 +1,25 @@
+//axios.defaults.headers.common['x-api-key'] = "d8927754-2985-4779-a3f5-f454e27cdd70"
+const url = 'https://api.thecatapi.com/v1/images/search';
+//https://thecatapi.com/v1/images? 
+api_key = "d8927754-2985-4779-a3f5-f454e27cdd70";
+let generate_btn = document.querySelector(".generate_btn");
+
+generate_btn.addEventListener("click", catsImg);
+
+function catsImg() {
+  let catsImgDiv = document.querySelector(".catsImgDiv")
+  catsImgDiv.innerHTML=''
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      let catsImgUrl = data[0].url;
+      let catsImgEl = document.createElement("img")
+      catsImgEl.setAttribute('src', `${catsImgUrl}`)
+      catsImgEl.classList.add("showcase")
+
+      let catsImgDiv = document.querySelector(".catsImgDiv")
+      catsImgDiv.appendChild(catsImgEl)
+    })
+    .catch(err => console.log(err))
+}
